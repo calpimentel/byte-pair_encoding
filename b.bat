6a9778df18a9d80fabbdf335584591459013ae20
calpimentel <34192318+calpimentel@users.noreply.github.com>
2023-07-25 21:01:34 -0300

    Add files via upload

---
+++ b.bat
@@ -0,0 +1,1 @@
+g++ decompress.cpp -o d:"\Mestrado\Analise de Algoritmos\Algoritmo\decompress.exe" -Wall -Wextra -g3  -Wl,--stack,73400320
---
+++ c.bat
@@ -0,0 +1,1 @@
+g++ compress.cpp -o d:"\Mestrado\Analise de Algoritmos\Algoritmo\compress.exe" -Wall -Wextra -g3  -Wl,--stack,73400320
